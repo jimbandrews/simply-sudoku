d45fa9b8693b7bba943dcfe380a1590e41057a55
James Andrews <jbandrew14@gmail.com>
2023-09-01 09:39:38 -0400

    added SudokuTable component

---
+++ src/components/SudokuTable.jsx
@@ -0,0 +1,16 @@
+import Table from "react-bootstrap/Table";
+
+function SudokuTable() {
+
+  return (
+    <Table>
+      <tbody>
+        <tr>
+
+        </tr>
+      </tbody>
+    </Table>
+  )
+};
+
+export default SudokuTable;
